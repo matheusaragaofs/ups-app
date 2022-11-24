d3563a1d65553f7f8742be84a0fa255081604fd9
matheus_aragao <matheusaragaofs@gmail.com>
2022-11-24 07:13:08 -0300

    setting up react native navigation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -10,7 +11,9 @@ export default function App() {
   return (
     //@ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <CustomersScreen/>
+      <NavigationContainer>
+        <CustomersScreen />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,14 +1,15 @@
-import { View, Text, SafeAreaView  } from 'react-native'
+import { Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import { useTailwind } from 'tailwind-rn/dist'
 
 const CustomersScreen = () => {
     const tw = useTailwind()
-  return (
-    <SafeAreaView>
-      <Text style={tw('text-red-500')}>CustomersScreen !!</Text>
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView>
+            <Text style={tw('text-red-500')}>Customers !!</Text>
+        </SafeAreaView>
+
+    )
 }
 
 export default CustomersScreen
