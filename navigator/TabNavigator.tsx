e968f52de96d3b34f3a0e2c8505ce4aed3376b19
matheus_aragao <matheusaragaofs@gmail.com>
2022-11-25 14:18:33 -0300

    removing header

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomersScreen from '../screens/CustomersScreen';
 import OrdersScreen from '../screens/OrdersScreen';
+import { useNavigation } from '@react-navigation/native';
 
-const Tab = createBottomTabNavigator()
+export type TabStackParamList = {
+  Customers : undefined;
+  Orders: undefined
+}
+
+const Tab = createBottomTabNavigator<TabStackParamList>()
 const TabNavigator = () => {
+  const navigation = useNavigation()
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+
   return (
     <Tab.Navigator>
         <Tab.Screen name='Customers' component={CustomersScreen}/>
