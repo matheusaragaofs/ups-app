964d85f44e96010254acf026f1796eb23b129509
matheus_aragao <matheusaragaofs@gmail.com>
2022-11-25 14:40:26 -0300

    adding icons to bottom tab navigation

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -4,16 +4,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomersScreen from '../screens/CustomersScreen';
 import OrdersScreen from '../screens/OrdersScreen';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from '@rneui/themed'
 
 export type TabStackParamList = {
-  Customers : undefined;
+  Customers: undefined;
   Orders: undefined
 }
 
 const Tab = createBottomTabNavigator<TabStackParamList>()
 const TabNavigator = () => {
   const navigation = useNavigation()
-  
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false
@@ -21,9 +22,33 @@ const TabNavigator = () => {
   }, [])
 
   return (
-    <Tab.Navigator>
-        <Tab.Screen name='Customers' component={CustomersScreen}/>
-        <Tab.Screen name='Orders' component={OrdersScreen}/>
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarActiveTintColor: "#59C1CC",
+      tabBarInactiveTintColor: "gray",
+      tabBarIcon: ({ focused, color, size }) => {
+        if (route.name === 'Customers') {
+          return (
+            <Icon
+             name='users'
+             type='entypo'
+             color={focused? '#59C1CC': 'gray' }
+            />
+          )
+        }else {
+          return (
+            <Icon
+             name='box'
+             type='entypo'
+             color={focused? '#EB6A7C': 'gray' }
+            />
+          )
+
+
+        }
+      }
+    })}>
+      <Tab.Screen name='Customers' component={CustomersScreen} />
+      <Tab.Screen name='Orders' component={OrdersScreen} />
     </Tab.Navigator>
   )
 }
