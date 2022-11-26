2e6515510745b8f5d003dde73c783fe7e7c5bec8
matheus aragao] <matheusaragaofs@gmail.com>
2022-11-26 12:07:46 -0300

    setting up appolo client

--- App.tsx
+++ App.tsx
@@ -3,15 +3,23 @@ import { StyleSheet } from 'react-native';
 import { TailwindProvider } from 'tailwind-rn';
 import RootNavigator from './navigator/RootNavigator';
 import utilities from './tailwind.json';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/bald-woodpecker',
+  cache: new InMemoryCache(),
+});
 
 
 export default function App() {
   return (
     //@ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
