8266b9b84f83f34da07c7b210bb67a1a779c35ec
matheus aragao] <matheusaragaofs@gmail.com>
2022-11-27 08:04:50 -0300

    building order screen

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,11 +1,57 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useLayoutEffect } from 'react'
+import { CompositeNavigationProp, RouteProp, useNavigation } from '@react-navigation/native'
+import { TabStackParamList } from '../navigator/TabNavigator'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../navigator/RootNavigator'
+import useOrders from '../hooks/useOrders'
+import { ScrollView, ActivityIndicator } from 'react-native'
+import { Button, Image } from '@rneui/themed'
+import { useTailwind } from 'tailwind-rn/dist'
+
+type Props = {
+
+}
+
+export type OrdersScreenNavigationProps = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, 'Orders'>,
+  NativeStackNavigationProp<RootStackParamList>
+>
 
 const OrdersScreen = () => {
+  const navigation = useNavigation<OrdersScreenNavigationProps>()
+  const { loading, error, orders } = useOrders()
+  const [ascending, setAscending] = useState<boolean>(false)
+  const tw = useTailwind()
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({ focused, color }) => (
+        <Text style={{ color: focused ? "#eb6a7c" : color, fontSize: 10 }}>
+          Orders
+        </Text>
+      )
+    })
+  }, [])
+
   return (
-    <View>
-      <Text>OrdersScreen</Text>
-    </View>
+    <ScrollView style={{backgroundColor: "#eb6a7c"}}>
+      <Image
+        source={{ uri: 'https:links.papareact.com/m51' }}
+        containerStyle={tw('w-full h-64')}
+        PlaceholderContent={<ActivityIndicator/>}
+      />
+      <View>
+        <Button 
+        color='pink'
+        titleStyle={{color:"gray", fontWeight:'400'}}
+        style={tw("py-2 px-5")}
+        onPress={()=> setAscending(!ascending)}>
+          {ascending ? "Showing: Oldest First" : "Showing: Most Recent First"}
+        </Button>
+      </View>
+    </ScrollView>
   )
 }
 
